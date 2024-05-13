dbeff75105d4d96e0b176d3a7a27ce88d6a67dd8
Jeffrey <oseremeosamor@gmail.com>
2024-05-13 19:05:28 -0400

    remove the .git file in the folders

---
+++ getting_started/local.tf
@@ -0,0 +1,4 @@
+
+locals {
+  project_name = "Tech_project"
+}
---
+++ provisioners/aws-security-group.tf
@@ -0,0 +1,35 @@
+resource "aws_security_group" "project_server_1_sg" {
+  name        = "project_server_1_sg"
+  description = "project_server_1 security group"
+  vpc_id      = "vpc-0f212443905033eff"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "SSH" {
+  security_group_id = aws_security_group.project_server_1_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+  description       = "SSH"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "HTTP" {
+  security_group_id = aws_security_group.project_server_1_sg.id
+  cidr_ipv6         = "::/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+  description       = "HTTP"
+}
+
+resource "aws_vpc_security_group_egress_rule" "project_server_1_sg_ipv4" {
+  security_group_id = aws_security_group.project_server_1_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+resource "aws_vpc_security_group_egress_rule" "project_server_1_sg_ipv6" {
+  security_group_id = aws_security_group.project_server_1_sg.id
+  cidr_ipv6         = "::/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
