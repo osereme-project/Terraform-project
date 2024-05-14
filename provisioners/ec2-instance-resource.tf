20355f11a7bd5d00bcd27cbfd65e75bb4cb8746a
Jeffrey <oseremeosamor@gmail.com>
2024-05-14 00:14:32 -0400

    ignore changes

--- provisioners/ec2-instance-resource.tf
+++ provisioners/ec2-instance-resource.tf
@@ -2,8 +2,8 @@ resource "aws_instance" "project_server_1" {
   ami                    = "ami-04b70fa74e45c3917"
   instance_type          = "t2.micro"
   key_name               = aws_key_pair.deployer.key_name
-  vpc_security_group_ids = [aws_vpc.my-first-vpc]
-  #  vpc_security_group_ids = aws_security_group.project_server_1_sg
+ # vpc_ids = "aws_vpc.my-first-vpc"
+  vpc_security_group_ids = aws_security_group.project_server_1_sg
   user_data = data.template_file.user_data
 
   # Using variables
